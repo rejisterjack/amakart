aed7575160f6e88ecd735823f2ba674f68b2f4d0
Rupam Das <83290469+rejisterjack@users.noreply.github.com>
2023-03-09 23:12:31 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import React from "react";
+export default function App() {
+  return React.createElement(
+    "div",
+    { className: "App" },
+    React.createElement("h1", {}, "hello world")
+  );
+}
