c13f08c9b3662ac1b576e8721136f6a953190d58
Rupam Das <83290469+rejisterjack@users.noreply.github.com>
2023-03-09 23:19:11 +0530

    explore about react components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./styles.css";
 import React from "react";
 export default function App() {
-  return React.createElement(
-    "div",
-    { className: "App" },
-    React.createElement("h1", {}, "hello world")
-  );
+
+  // react components 
+
+  // return React.createElement(
+  //   "div",
+  //   { className: "App" },
+  //   React.createElement("h1", {}, "hello world")
+  // );
+
+  return (
+    <>
+    
+    </>
+  )
 }
