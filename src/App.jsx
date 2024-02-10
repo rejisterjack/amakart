c9c05bcd75ad3c4e634a1e05fbbcb5ba456446b7
Rupam Das <rejisterjack@gmail.com>
2024-02-10 12:41:15 +0530

    styling fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import React from "react"
-
 const App = () => {
-  return <div>App</div>
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam doloremque
+      dolorum necessitatibus reprehenderit voluptas, exercitationem similique
+      delectus consectetur fugiat possimus cum alias excepturi esse animi
+      impedit nulla inventore quo cumque in ducimus provident eaque quam
+      voluptatum. Expedita laudantium laborum distinctio dicta quasi libero quo
+      delectus sit iure! Velit, perspiciatis quisquam?
+    </div>
+  )
 }
 
 export default App
